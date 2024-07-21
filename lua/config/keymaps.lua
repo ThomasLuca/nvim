f5f98b60b0b772860b71e323bf09ef9e8f78bee7
Thomas Luca <thomasluca008@gmail.com>
2024-07-21 16:04:20 +0200

    Custom keybinds and inproved nav

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,9 +3,11 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<space>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-vim.api.nvim_set_keymap("n", "TT", ":TransparentToggle<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<space>tt", ":TransparentToggle<CR>", { noremap = true })
 
 vim.api.nvim_set_keymap("n", "gl", "$", { noremap = false })
 vim.api.nvim_set_keymap("v", "gl", "$", { noremap = false })
 vim.api.nvim_set_keymap("n", "gh", "^", { noremap = false })
 vim.api.nvim_set_keymap("v", "gh", "^", { noremap = false })
+
+vim.keymap.set("n", "U", "<C-r>", { noremap = true, desc = "Redo" })
