03518e06f409e45b79bb634432aeb88bb9a51c16
Thomas Luca <thomasluca008@gmail.com>
2024-07-21 16:03:47 +0200

    Set custom neovim options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.o.colorcolumn = "79"
+
+vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.expandtab = true
+vim.o.scrolloff = 5
+vim.o.ignorecase = true
+vim.o.smarttab = true
+vim.o.breakindent = true
+vim.o.backup = false
